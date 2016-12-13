94788e3d05fa4fcb7047475ccab8b9c190933841
Dmitriy Khokhlov <dh@choose4use.com>
2016-12-13 14:15:48 +0300

    add test project

---
+++ Binding.Client.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Newrelic.Agent.Android;
+
+namespace Binidng.Client.Android
+{
+    [Activity(Label = "Binidng.Client.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            //NewRelicHelper.withApplicationToken("").start(this.Application);
+            //NewRelicHelper - is not available
+            //NewRelic - is not available
+            // How to initialize newrelic?
+        }
+    }
+}
+
